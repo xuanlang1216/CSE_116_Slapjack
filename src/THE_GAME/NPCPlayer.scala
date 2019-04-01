1e98c9389c92084da1cbbf7d6aa02965e4dd8718
xuanlang1216 <xuanlang1216@gmail.com>
2019-03-31 23:47:39 -0400

    single

--- src/THE_GAME/NPCPlayer.scala
+++ src/THE_GAME/NPCPlayer.scala
@@ -9,7 +9,7 @@ class NPCPlayer extends Player {
         val system = akka.actor.ActorSystem("system")
         import system.dispatcher
         system.scheduler.scheduleOnce(3000 milliseconds) {
-
+        LastSlaptime += math.random()*100+2
         }
     }
 
